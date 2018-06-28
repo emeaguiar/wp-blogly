d35a3f440e19b82c5ec1b4800a419d0edb8a42f6
Mario Aguiar <mario@10up.com>
2018-06-28 14:10:21 -0500

    Add basic nav container

---
+++ partials/header.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Site header
+ */
+?>
+
+<div
+	class="site-header"
+	id="main-header"
+	role="banner"
+	itemscope="itemscope"
+	itemtype="http://schema.org/WPHeader"
+>
+	<div class="wrapper">
+
+	</div>
+</div>

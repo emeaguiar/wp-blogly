f34ccbc1d98eaeb39e9427c50971e222f7d1bc4d
Mario Aguiar <mario@10up.com>
2018-06-28 14:48:05 -0500

    Display left menu

--- partials/header.php
+++ partials/header.php
@@ -12,6 +12,14 @@
 	itemtype="http://schema.org/WPHeader"
 >
 	<div class="wrapper">
-
+		<div class="navigation -left">
+			<?php
+			wp_nav_menu( [
+				'theme_location' => 'header_left',
+			] );
+			?>
+		</div>
+		<div class="navigation -center"></div>
+		<div class="navigation -right"></div>
 	</div>
 </div>

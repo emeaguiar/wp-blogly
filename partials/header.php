0e705688e628bedc6ec7f2548d70f9780f7c565a
Mario Aguiar <mario@10up.com>
2018-06-29 12:35:54 -0500

    update header

--- partials/header.php
+++ partials/header.php
@@ -4,7 +4,7 @@
  */
 ?>
 
-<div
+<header
 	class="site-header"
 	id="main-header"
 	role="banner"
@@ -12,16 +12,16 @@
 	itemtype="http://schema.org/WPHeader"
 >
 	<div class="wrapper">
-		<div class="navigation -side -left">
+		<div class="navigation-menu -side -left">
 			<?php
 			wp_nav_menu( [
 				'theme_location' => 'header_left',
 				'container'      => 'ul',
-				'menu_class'     => 'actions-menu',
+				'menu_class'     => 'actions-menu menu',
 			] );
 			?>
 		</div>
-		<div class="navigation -center">
+		<div class="navigation-menu -center">
 			<?php
 			if ( has_custom_logo() ) {
 				the_custom_logo();
@@ -30,23 +30,23 @@
 			}
 			?>
 		</div>
-		<div class="navigation -side -right">
+		<div class="navigation-menu -side -right">
 			<?php
 			wp_nav_menu( [
 				'theme_location' => 'header_right',
 				'container'      => 'ul',
-				'menu_class'     => 'notifications-menu',
+				'menu_class'     => 'notifications-menu menu',
 			] );
 			?>
 		</div>
-		<nav class="navigation -bottom" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
+		<nav class="navigation-menu -bottom" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
 			<?php
 			wp_nav_menu( [
 				'theme_location' => 'header_bottom',
 				'container'      => 'ul',
-				'menu_class'     => 'primary-menu',
+				'menu_class'     => 'primary-menu menu',
 			] );
 			?>
 		</nav>
 	</div>
-</div>
+</header>

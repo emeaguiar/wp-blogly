744292fbba98ca7eff3454812a06a95e86c92639
Mario Aguiar <mario@10up.com>
2018-06-29 15:01:35 -0500

    Add icons to menu

--- partials/nav-actions.php
+++ partials/nav-actions.php
@@ -5,10 +5,10 @@
 ?>
 
 <ul class="actions-menu menu">
-	<li class="action">
+	<li class="action -share">
 		<a href="#" class="link"><?php esc_html_e( 'Share', 'blogly' ); ?></a>
 	</li>
-	<li class="action">
+	<li class="action -search">
 		<a href="#" class="link"><?php esc_html_e( 'Search', 'blogly' ); ?></a>
 	</li>
 </ul>
--- partials/nav-notifications.php
+++ partials/nav-notifications.php
@@ -4,11 +4,11 @@
  */
 ?>
 
-<ul class="notifications-menu menu">
-	<li class="action">
+<ul class="actions-menu menu">
+	<li class="action -notification">
 		<a href="#" class="link"><?php esc_html_e( 'Notifications', 'blogly' ); ?></a>
 	</li>
-	<li class="action">
+	<li class="action -favorite">
 		<a href="#" class="link"><?php esc_html_e( 'Favorite', 'blogly' ); ?></a>
 	</li>
 </ul>

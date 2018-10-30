6a10a93aed64a29d0024c9211056e3a116e5089b
Mario Aguiar <mario@10up.com>
2018-10-30 12:40:19 -0600

    Add markup for home page

--- index.php
+++ index.php
@@ -5,11 +5,17 @@
 
 get_header(); ?>
 
-	<?php if ( have_posts() ) : ?>
-		<?php while ( have_posts() ): the_post(); ?>
-			<h2><?php the_title(); ?></h2>
-			<?php the_content(); ?>
-		<?php endwhile; ?>
-	<?php endif; ?>
+<main class="site-content" id="content">
+	<div class="page-container -narrow">
+		<?php if ( have_posts() ) : ?>
+			<?php while ( have_posts() ): the_post(); ?>
+				<h2>
+					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+				</h2>
+				<?php the_content(); ?>
+			<?php endwhile; ?>
+		<?php endif; ?>
+	</div>
+</main>
 
 <?php get_footer();

678a00ee880b0108ed9757344627893b6225e9e2
Mario Aguiar <mario@10up.com>
2018-10-30 13:01:51 -0600

    Style article list

--- index.php
+++ index.php
@@ -8,12 +8,19 @@ get_header(); ?>
 <main class="site-content" id="content">
 	<div class="page-container -narrow">
 		<?php if ( have_posts() ) : ?>
-			<?php while ( have_posts() ): the_post(); ?>
-				<h2>
-					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-				</h2>
-				<?php the_content(); ?>
-			<?php endwhile; ?>
+			<div class="article-list">
+				<?php while ( have_posts() ): the_post(); ?>
+					<div class="item">
+						<a href="<?php the_permalink(); ?>" class="link">
+							<h2 class="title">
+								<?php the_title(); ?>
+							</h2>
+							<time class="date"><?php the_time( 'j F Y' ); ?></time>
+						</a>
+					</div>
+					<?php the_content(); ?>
+				<?php endwhile; ?>
+			</div>
 		<?php endif; ?>
 	</div>
 </main>
